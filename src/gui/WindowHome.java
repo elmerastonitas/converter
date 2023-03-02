a7daac3ebbe901210f546b4b016dcb9a8d5be155
Elmer Astonitas <elmerastonitas@gmail.com>
2023-03-01 22:36:55 -0500

    Diseño inicial de convertidor

--- src/gui/WindowHome.java
+++ src/gui/WindowHome.java
@@ -5,6 +5,12 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 public class WindowHome extends JFrame {
 
@@ -31,11 +37,45 @@ public class WindowHome extends JFrame {
 	 */
 	public WindowHome() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 600, 400);
+		
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setToolTipText("");
+		setJMenuBar(menuBar);
+		
+		JMenu mnNewMenu = new JMenu("Inicio");
+		menuBar.add(mnNewMenu);
+		
+		JMenu mnNewMenu_1 = new JMenu("Convertir");
+		menuBar.add(mnNewMenu_1);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("Monedas");
+		mnNewMenu_1.add(mntmNewMenuItem);
+		
+		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Temperatura");
+		mnNewMenu_1.add(mntmNewMenuItem_1);
+		
+		JMenuItem mntmNewMenuItem_2 = new JMenuItem("Tiempo");
+		mnNewMenu_1.add(mntmNewMenuItem_2);
+		
+		JMenuItem mntmNewMenuItem_3 = new JMenuItem("Longitud");
+		mnNewMenu_1.add(mntmNewMenuItem_3);
+		
+		JMenuItem mntmNewMenuItem_4 = new JMenuItem("Masa");
+		mnNewMenu_1.add(mntmNewMenuItem_4);
+		
+		JMenu mnNewMenu_2 = new JMenu("Ayuda");
+		menuBar.add(mnNewMenu_2);
+		
 		PanelHome = new JPanel();
 		PanelHome.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(PanelHome);
+		PanelHome.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblNewLabel = new JLabel("Powered by Elmer Astonitas");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		PanelHome.add(lblNewLabel, BorderLayout.SOUTH);
 	}
 
 }
