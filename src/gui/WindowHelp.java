401bc2596393a5be6cba0e50d8cfca429d271d0e
Elmer Astonitas <elmerastonitas@gmail.com>
2023-03-02 02:46:24 -0500

    Mejoras en el diseño de interfaces

--- src/gui/WindowHelp.java
+++ src/gui/WindowHelp.java
@@ -11,12 +11,14 @@ import javax.swing.SwingConstants;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
-import java.awt.Font;
+import javax.swing.JTextPane;
+import java.awt.FlowLayout;
+import javax.swing.JSeparator;
 import java.awt.Toolkit;
 
-public class WindowHome extends JFrame {
+public class WindowHelp extends JFrame {
 
-	private JPanel PanelHome;
+	private JPanel PanelHelp;
 
 	/**
 	 * Launch the application.
@@ -25,7 +27,7 @@ public class WindowHome extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					WindowHome frame = new WindowHome();
+					WindowHelp frame = new WindowHelp();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -37,39 +39,44 @@ public class WindowHome extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public WindowHome() {
-		setTitle("ConverterFull - Inicio");
+	public WindowHelp() {
+		setTitle("ConverterFull - Inicio/Acerca de");
 		setIconImage(Toolkit.getDefaultToolkit().getImage("D:\\eclipse-workspace\\converter\\src\\imgs\\converter.png"));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 600, 400);
-		
+
 		JMenuBar menuBar = new JMenuBar();
-		menuBar.setToolTipText("");
 		setJMenuBar(menuBar);
-		
+
 		JMenu mnNewMenu = new JMenu("Inicio");
 		menuBar.add(mnNewMenu);
-		
+
 		JMenu mnNewMenu_1 = new JMenu("Convertir");
 		menuBar.add(mnNewMenu_1);
-		
+
 		JMenu mnNewMenu_2 = new JMenu("Acerca de");
 		menuBar.add(mnNewMenu_2);
-		
-		PanelHome = new JPanel();
-		PanelHome.setBorder(new EmptyBorder(5, 5, 5, 5));
+		PanelHelp = new JPanel();
+		PanelHelp.setBorder(new EmptyBorder(5, 5, 5, 5));
 
-		setContentPane(PanelHome);
-		PanelHome.setLayout(new BorderLayout(0, 0));
-		
+		setContentPane(PanelHelp);
+		PanelHelp.setLayout(new BorderLayout(0, 0));
+
 		JLabel lblNewLabel = new JLabel("Powered by Elmer Astonitas");
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		PanelHome.add(lblNewLabel, BorderLayout.SOUTH);
-		
-		JLabel lblNewLabel_1 = new JLabel("¡Bienvenido/a al ConverterFull!");
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-		PanelHome.add(lblNewLabel_1, BorderLayout.CENTER);
-	}
+		PanelHelp.add(lblNewLabel, BorderLayout.SOUTH);
 
+		JPanel panel = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) panel.getLayout();
+		flowLayout.setAlignment(FlowLayout.LEADING);
+		PanelHelp.add(panel, BorderLayout.CENTER);
+
+		JLabel lblNewLabel_1 = new JLabel(
+				"<html><body><br><strong>Convertidor de monedas, tiempo, masa, temperatura, longitud y más</strong> <br><br> Versión: 2023-03 (1.0.0) <br> Copyright (c) 2023 Elmer Astonitas <br><br> Este software esta desarrollado bajo licencia de código abierto MIT License.<br><br> Codigo disponible en GitHub: https://github.com/elmerastonitas/converter</body></html>");
+		panel.add(lblNewLabel_1);
+
+		JSeparator separator = new JSeparator();
+		separator.setOrientation(SwingConstants.VERTICAL);
+		panel.add(separator);
+	}
 }
